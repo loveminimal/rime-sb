3a8dea37b1f7fec2ce63c4bfb0a26317b34b10cb
Jack Liu <loveminimal@163.com>
2026-07-10 14:31:07 +0800

    feat: force_gc lua

--- lua/patch.lua
+++ lua/patch.lua
@@ -152,9 +152,21 @@ local function f_filter_non_first_word(input, env)
     end    
 end
 
+-- 暴力 GC
+-- 详情 https://github.com/hchunhui/librime-lua/issues/307
+-- collectgarbage()：默认调用，等同于 collectgarbage("collect")，触发完整的垃圾回收。
+-- collectgarbage("step")：执行垃圾回收的一小步。这个函数会返回一个布尔值，表示这一步是否完成了整个收集周期。
+-- 这样也不会导致卡顿，那就每次都调用一下吧，内存稳稳的
+local function t_force_gc()
+    -- collectgarbage()
+    collectgarbage("step")
+end
+
+
 return {
     -- p_lower_first_char = p_lower_first_char,
     t_date = t_date,
+    t_force_gc = t_force_gc,
     f_auto_select = f_auto_select,
     f_comment = f_comment,
     -- f_filter_non_first_word = f_filter_non_first_word,
